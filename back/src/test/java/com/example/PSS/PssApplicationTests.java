ceb81f6c48b9b9160d5c3c070dd6a0b343510516
artem <artem_berezhnov@mail.ru>
2023-02-10 15:56:28 +0300

    init Java Spring project

---
+++ back/src/test/java/com/example/PSS/PssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

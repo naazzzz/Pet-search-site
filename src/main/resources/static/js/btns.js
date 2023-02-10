0cb099d3e2ccd12bf6afc125c0a4bb5057f2e50f
artem <artem_berezhnov@mail.ru>
2023-02-11 02:25:48 +0300

    переместил в res

--- src/main/resources/static/js/btns.js
+++ src/main/resources/static/js/btns.js
@@ -35,7 +35,20 @@ else{
 
 document.getElementById('btn_next').addEventListener('click',function(){
     if(qu1 && qu2){
-    document.location='/front/html/wH_form2.html';
+        let params = window
+            .location
+            .search
+            .replace('?','')
+            .split('&')
+            .reduce(
+                function(p,e){
+                    let a = e.split('=');
+                    p[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
+                    return p;
+                },
+                {}
+            );
+        document.location='/MainWindow/CreateAd?form='+ params['form'] +'&step=2';
     }
     else{
         alert("Для перехода дальше выберите все подпункты в форме")

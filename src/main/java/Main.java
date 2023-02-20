828de89e27ad74478eabbfbbbca4543775a1fbc1
artem <artem_berezhnov@mail.ru>
2023-02-21 01:55:23 +0300

    reconfig

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
+import jakarta.persistence.metamodel.Metamodel;
 import org.hibernate.HibernateException;
-import org.hibernate.Metamodel;
 import org.hibernate.query.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import javax.persistence.metamodel.EntityType;
+import jakarta.persistence.metamodel.EntityType;
 
 import java.util.Map;
 
